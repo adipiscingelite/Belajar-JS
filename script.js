189a57b62686c915e94e626c55987f39757812e0
adipiscingelite <syaifulrahmatdani.18>
2024-02-07 12:57:59 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// alert("Hello World")
+console.log("Hello World from console")
+
+const nama = "Syaiful"
+let umur = 19
+umur = 20
+
+console.log(nama + " umur " + umur)
+
+
+console.log(2 + 2)
+console.log(3 - 1)
+console.log(2 * 3)
+console.log(5 ** 2)
+console.log(6 / 2)
+console.log(7 % 2)
+
+let angka_dua = 2
+let angka_tiga = 3
+
+console.log(angka_dua + angka_tiga)
+console.log(angka_dua - angka_tiga)
+console.log(angka_dua * angka_tiga)
+console.log(angka_dua / angka_tiga)
+
+let x = 1
+x += 1
+console.log(x)
+
+let y = 1
+y++
+console.log(y)
